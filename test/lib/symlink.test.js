78852ecf8a3454f577ce7be296156bbb04fd447a
Adam Duncan <adam.duncan@clock.co.uk>
2015-04-23 17:48:20 +0100

    Ensure buildDir exists before symlinking. Fail if not. Fixes #1

--- test/lib/symlink.test.js
+++ test/lib/symlink.test.js
@@ -5,33 +5,53 @@ var fs = require('fs')
 
 describe('symlink', function () {
 
-  before(function (done) {
-    fs.mkdir('/tmp/symlink-test', function () {
-      done()
-    })
-  })
+  describe('build dir exists', function () {
 
-  it('should create symlink', function (done) {
-    var emitSpy = sinon.spy()
-      , context = { emit: emitSpy }
-      , data = { buildDir: '/tmp/symlink-test', finalDir: '/tmp/symlink-test-symlink' }
-
-    symlink(context, data, function (error) {
-      should.not.exist(error)
-      emitSpy.calledOnce.should.equal(true)
-      fs.exists('/tmp/symlink-test-symlink', function (exists) {
-        exists.should.equal(true)
+    before(function (done) {
+      fs.mkdir('/tmp/symlink-test', function () {
         done()
       })
     })
+
+    it('should create symlink', function (done) {
+      var emitSpy = sinon.spy()
+        , context = { emit: emitSpy }
+        , data = { buildDir: '/tmp/symlink-test', finalDir: '/tmp/symlink-test-symlink' }
+
+      symlink(context, data, function (error) {
+        should.not.exist(error)
+        emitSpy.calledOnce.should.equal(true)
+        fs.exists('/tmp/symlink-test-symlink', function (exists) {
+          exists.should.equal(true)
+          done()
+        })
+      })
+    })
+
+    after(function (done) {
+      fs.rmdir('/tmp/symlink-test/', function () {
+        fs.unlink('/tmp/symlink-test-symlink', function () {
+          done()
+        })
+      })
+    })
+
   })
 
-  after(function (done) {
-    fs.rmdir('/tmp/symlink-test/', function () {
-      fs.unlink('/tmp/symlink-test-symlink', function () {
+  describe('build dir does not exist', function () {
+
+    it('should fail to create symlink', function (done) {
+      var emitSpy = sinon.spy()
+        , context = { emit: emitSpy }
+        , data = { buildDir: '/tmp/not-exist', finalDir: '/tmp/symlink-test-symlink' }
+
+      symlink(context, data, function (error) {
+        should.exist(error)
+        error.message.should.equal('Build directory does not exist: /tmp/not-exist')
         done()
       })
     })
+
   })
 
 })
